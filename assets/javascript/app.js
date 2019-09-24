aae55fa224f13ef40370b74263d62ed7e15ce8d9
linmao0991 <lin.maoxian0991@gmail.com>
2019-09-23 23:56:50 -0400

    Added html code, js code, and css code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,24 +2,85 @@ var topics = ["anaconda", "boar", "cat", "dog", "elephant", "fish", "giraffe", "
 var apiKey = "ShmyFeKw9JQGtRlJkQypZSgpphZCyW6T";
 var resultLimit = 10;
 var resultRating = "G";
+
+//On window load event to create buttons for all the items in the array topics
 $(window).on("load", function(){
-    for ( var i = 0; i < topics.length; i++){
-        var button = $("<button>");
-        button.attr("type", button);
-        button.attr("data-topic", topics[i]);
-        button.addClass("topicButton btn btn-info m-2");
-        button.text(topics[i]);
-        $("#buttonColumn").prepend(button);
+    if(localStorage.storedTopic){
+        topics = localStorage.storedTopic.split(",");
+        for ( var i = 0; i < topics.length; i++){
+            createButton(topics[i]);
+        }
+        console.log("Loading stored session storage");
+        console.log(localStorage.storedTopic);
+    }else{
+        localStorage.setItem("storedTopic",topics);
+        for ( var i = 0; i < topics.length; i++){
+            createButton(topics[i]);
+        }
+        console.log("Create new session storage");
+        console.log(localStorage.storedTopic);
     }
 });
 
+//On click function listining to id addTopic to add the input as a button.
+$(document).on("click", "#addTopic", function(){
+    var newTopic = $("#newTopic").val().trim().toLowerCase();
+    if(topics.indexOf(newTopic) == -1 && newTopic != ""){
+    createButton(newTopic);
+    topics.push(newTopic);
+    localStorage.setItem("storedTopic",topics);
+    $("#newTopic").val("");
+    }else if (newTopic == ""){
+        //Do Nothing
+    }else{
+        alert("That topic is already in the list!");
+        $("#newTopic").val("");
+    }
+});
+
+$(document).on("click", "#clearLocalStorage", function(){
+    if(localStorage.storedTopic){
+        localStorage.removeItem("storedTopic");
+        topics = ["anaconda", "boar", "cat", "dog", "elephant", "fish", "giraffe", "hippo", "gorilla", "jackal", "iguana","llama","monkey","newt"];
+        $("#buttonColumn").html("");
+        for (var i = 0; i < topics.length; i++){
+            createButton(topics[i]);
+        }
+    }else{
+        alert("No local storage found.");
+    }
+});
+
+//On click function listining to class topicButton then calling displayResults to create gif cards 
 $(document).on("click",".topicButton", function(){
     $("#cardColumns").html("");
     var searchTopic = $(this).attr("data-topic");
-    console.log(searchTopic);
     displayResults(searchTopic);
 });
 
+//Function to create buttons for loading topic gifs
+function createButton(topicString){
+    var button = $("<button>");
+    button.attr("type", button);
+    button.attr("data-topic", topicString);
+    button.addClass("topicButton btn btn-info m-2");
+    button.text(topicString);
+    $("#buttonColumn").prepend(button);
+};
+
+//Function to pause and play gif
+$(document).on("click",".card-img-top", function(){
+    var animate = $(this).attr("data-animate");
+    if( animate == "pause"){
+    $(this).attr("data-animate","play");
+    $(this).attr("src",$(this).attr("data-gif-play"));
+    }else{
+        $(this).attr("data-animate","pause");
+        $(this).attr("src",$(this).attr("data-gif-pause"));
+    }
+});
+
+//Function to create card elements for each gif.
 function displayResults(passedTopic){
     var searchTopic = passedTopic;
     queryURL = "https://api.giphy.com/v1/gifs/search?api_key="+apiKey+"&q="+searchTopic+"&limit="+resultLimit+"&offset=0&rating="+resultRating+"&lang=en"
@@ -34,13 +95,16 @@ function displayResults(passedTopic){
             var card = $("<div>").addClass("card");
             //Card Image
             var cardImg = $("<img>").addClass("card-img-top");
-            cardImg.attr("src",response.data[i].images.fixed_height.url);
+            cardImg.attr("src",response.data[i].images.fixed_height_still.url);
             cardImg.attr("alt",searchTopic+i);
+            cardImg.attr("data-gif-pause",response.data[i].images.fixed_height_still.url)
+            cardImg.attr("data-gif-play",response.data[i].images.fixed_height.url);
+            cardImg.attr("data-animate","pause");
             card.append(cardImg);
             //Card Body
             var cardBody = $("<div>").addClass("card-body");
-            var cardBodyTitle = $("<h5>").addClass("card-title");
-            cardBodyTitle.text(searchTopic);
+            var cardBodyTitle = $("<h5>").addClass("card-title border rounded p-1");
+            cardBodyTitle.text(response.data[i].title);
             var cardBodyText = $("<p>").addClass("card-text");
             cardBodyText.html("The above image is rated: <b>"+response.data[i].rating.toUpperCase()+"</b>");
             cardBody.append(cardBodyTitle);
